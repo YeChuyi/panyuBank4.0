aa625b1f0a14b72162c4c26f29a837a3d8e6143b
yechuyi <791098535@qq.com>
2019-09-11 15:31:22 +0800

    更新新接口。你看看对不对、、

--- src/main/java/com/bank/controller/HelloController.java
+++ src/main/java/com/bank/controller/HelloController.java
@@ -1,9 +1,19 @@
 package com.bank.controller;
 
 
+import com.bank.bean.PeLoOrder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
     //ÄãºÃ£¬¿ØÖÆ²ã
+
+    @RequestMapping("/hello")
+    public String personalDetail(@RequestParam(value = "id") String id, Map<String, Object> map){
+        return "hello";
+    }
 }
